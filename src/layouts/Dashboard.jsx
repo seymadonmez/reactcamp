753694d99c438ab0c6d15b7c19b082846ac11297
seymadonmez <77541232+seymadonmez@users.noreply.github.com>
2021-06-06 13:11:34 +0300

    first commit

---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ProductList from "../pages/ProductList";
+import Categories from "./Categories";
+import { Grid, GridColumn } from "semantic-ui-react";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Grid>
+        <Grid.Row>
+          <GridColumn width={4}>
+            <Categories />
+          </GridColumn>
+          <GridColumn width={12}>
+            <ProductList />
+          </GridColumn>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
+}
